8399364f7dc23452c1968de56c036beda0520759
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-11-08 09:03:55 -0500

    comment out lorem text on dashboard page

--- src/modules/dashboard/DashboardPage.tsx
+++ src/modules/dashboard/DashboardPage.tsx
@@ -5,13 +5,13 @@ export const DashboardPage = (props) => {
     return (
         <DashboardNavBar>
             <PieChart {...props} />
-            <p style={{ margin: "2rem" }}>
+            {/* <p style={{ margin: "2rem" }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, mollitia! Expedita, vero!
                 Consectetur incidunt facere omnis molestiae eligendi necessitatibus facilis ullam libero et accusantium
                 officiis at quod veniam voluptatibus ipsa, suscipit dolorem laborum repudiandae eius obcaecati magnam
                 vero qui! Reiciendis animi consectetur dolor perferendis. Dolores aliquam nam pariatur eligendi
                 necessitatibus?
-            </p>
+            </p> */}
         </DashboardNavBar>
     );
 };
