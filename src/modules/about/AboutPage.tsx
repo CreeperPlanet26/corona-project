f045f6e2554734176fc3c48efb93b7905493e2a9
CreeperPlanet26 <fortniteplanet26@gmail.com>
2021-11-20 11:54:10 -0500

    go back to high res video

--- src/modules/about/AboutPage.tsx
+++ src/modules/about/AboutPage.tsx
@@ -7,7 +7,7 @@ export const AboutPage = () => {
             <div className="about-page">
                 <h1>About</h1>
                 <video width="320" height="240" controls>
-                    <source src="assets/Corona_Video_Compressed.mp4" type="video/mp4" />
+                    <source src="assets/Corona_Video.mp4" type="video/mp4" />
                 </video>
             </div>
         </DashboardNavBar>
